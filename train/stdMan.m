37d31863a4e425a41bd45d1b5382b608eb316c68
Alif Irhas <irhasalif@gmail.com>
2021-11-30 13:47:52 +0700

    Std dan Mean col
    Menghitung std dan mean pada kolom

--- train/stdMan.m
+++ train/stdMan.m
@@ -1,4 +1,4 @@
-function [std] = stdMan(data, meanData)
+function [std] = stdMan(data, meanData, axis)
 %STDMAN Menghitung std row matrix secara manual 
 %   ! stdMan memerlukan mean dari semua row
 %   Standar deviasi disini menggunakan quadran
@@ -8,18 +8,38 @@ function [std] = stdMan(data, meanData)
 std = [];
 [row, col] = size(data);
 
-for ii = 1:col
-    dataRow = data(:,ii);
-    dataRowSize = length(dataRow);
-    sumRow = 0;
-    
-    for jj = 1:dataRowSize
-        sumRow = sumRow + (dataRow(jj)-meanData(ii))^2;
-    end
-    
-    meanRow = sumRow / row;
-    std = [std meanRow];
+if ~exist('axis','var')
+    axis = 0;
 end
+
+if axis == 0
+    for ii = 1:col
+        dataRow = data(:,ii);
+        dataRowSize = length(dataRow);
+        sumRow = 0;
+
+        for jj = 1:dataRowSize
+            sumRow = sumRow + (dataRow(jj)-meanData(ii))^2;
+        end
+        disp(sumRow);
+        meanRow = sumRow / (row-1);
+        std = [std meanRow];
+    end
+else
+    for ii = 1:row
+        dataCol = data(ii,:);
+        dataColSize = length(dataCol);
+        sumCol = 0;
+
+        for jj = 1:dataColSize
+            sumCol = sumCol + (dataCol(jj)-meanData(ii))^2;
+        end
+        disp(sumCol);
+        meanCol = sumCol / (col-1);
+        std = [std meanCol];
+    end    
+end
+
 
 
 end
