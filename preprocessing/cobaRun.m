9aa30907bad0bf638fed9b37a5c94e2c527d79a1
priska19 <priska360@gmail.com>
2022-01-01 13:59:18 +0700

    Update cobaRun.m

--- preprocessing/cobaRun.m
+++ preprocessing/cobaRun.m
@@ -1,10 +1,9 @@
-gambar = imread('belimbing.jpeg');
-gray = rgb2gray('gambar')
-resz = resize1 (gray)
+gambar = imread('tas.jpg');
+resz = resize1(gambar)
 rem = removeMan(resz)
 ekstraksi1 = ekstraksiMan (double(rem));
 
 subplot(1,3,3),imshow(rem);
 subplot(1,3,2),imshow(resz);
-subplot(1,3,1),imshow(gray);
+subplot(1,3,1),imshow(gambar);
 %imshow (rem);
